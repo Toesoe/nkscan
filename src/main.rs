093487e92ccc3a4e942d46aa476932d0f53ef6aa
Kiran Shila <me@kiranshila.com>
2026-07-19 18:48:17 -0700

    feat: fleshing out the SCSI abstraction
    We can talk to the scanner now!

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
+use crate::{scanners::Ls9k, scsi::linux::SgDevice};
+
+mod scanners;
 mod scsi;
 
 fn main() {
-    println!("Hello, world!");
+    let transport = SgDevice::open("/dev/sg4").unwrap();
+    let mut scanner = Ls9k::new(transport);
+    let resp = scanner.inquiry().unwrap();
+    println!("{resp:#?}");
 }
