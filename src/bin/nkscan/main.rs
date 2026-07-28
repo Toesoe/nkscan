729b4e9131ca763ac72de297329638ab117aca48
Kiran Shila <me@kiranshila.com>
2026-07-27 22:17:44 -0700

    few bugs from rename

--- src/bin/nkscan/main.rs
+++ src/bin/nkscan/main.rs
@@ -14,7 +14,7 @@ use ls9000::Ls9000Job;
 use nkscan::{
     decode::Image,
     output,
-    scanners::{ls50, ls5000},
+    scanners::{ls50 as ls50_scanner, ls5000 as ls5000_scanner},
     scsi::{Transport, TransportExt, usb::UsbTransport},
 };
 use nusb::MaybeFuture;
@@ -285,16 +285,16 @@ const MODELS: &[Model] = &[
     Model {
         name: "LS-50 ED",
         attach: Attach::Usb {
-            vendor: ls50::VENDOR_ID,
-            product: ls50::PRODUCT_ID,
+            vendor: ls50_scanner::VENDOR_ID,
+            product: ls50_scanner::PRODUCT_ID,
         },
         open: Ls50Job::open,
     },
     Model {
         name: "LS-5000 ED",
         attach: Attach::Usb {
-            vendor: ls5000::VENDOR_ID,
-            product: ls5000::PRODUCT_ID,
+            vendor: ls5000_scanner::VENDOR_ID,
+            product: ls5000_scanner::PRODUCT_ID,
         },
         open: Ls5000Job::open,
     },
